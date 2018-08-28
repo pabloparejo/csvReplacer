7c5c57fcddf2d4790ec0ebf6cc9bebd51294edfc
pabloparejo <parejo.p@gmail.com>
2018-08-28 18:24:44 +0200

    Replace regex and print output in console

--- script.py
+++ script.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 import argparse
 import csv
+import re
 
 parser = argparse.ArgumentParser()
 parser.add_argument("file", help="Name of file to transform", type=str)
@@ -9,6 +10,16 @@ args = parser.parse_args()
 
 with open(args.file, 'rb') as input_file:
     reader = csv.reader(input_file)
+
+    pattern = re.compile(r"e(\w)e")
+
     for row in reader:
         row.pop(2)
+        index = 0
+        for col in row:
+            row[index] = re.sub(pattern, r'a\1a', row[index])
+            index += 1
+            if index >= 2:
+                break
+
         print ','.join(row)
