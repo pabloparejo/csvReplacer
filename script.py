160b1ca842cc04fea7a9382a38e3a1658fa67912
pabloparejo <parejo.p@gmail.com>
2018-08-28 18:31:48 +0200

    write output to file

--- script.py
+++ script.py
@@ -9,17 +9,19 @@ parser.add_argument("file", help="Name of file to transform", type=str)
 args = parser.parse_args()
 
 with open(args.file, 'rb') as input_file:
-    reader = csv.reader(input_file)
+    with open('output.csv', 'wb') as output_file:
+        reader = csv.reader(input_file)
+        writer = csv.writer(output_file)
 
-    pattern = re.compile(r"e(\w)e")
+        pattern = re.compile(r"e(\w)e")
 
-    for row in reader:
-        row.pop(2)
-        index = 0
-        for col in row:
-            row[index] = re.sub(pattern, r'a\1a', row[index])
-            index += 1
-            if index >= 2:
-                break
+        for row in reader:
+            row.pop(2)
+            index = 0
+            for col in row:
+                row[index] = re.sub(pattern, r'a\1a', row[index])
+                index += 1
+                if index >= 2:
+                    break
 
-        print ','.join(row)
+            writer.writerow(row)
